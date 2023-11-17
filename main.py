e791fb50b68a672b4a9e2d5f9e0c5219b30522db
keda <keda@senseauto.com>
2023-11-17 13:20:33 +0800

    Bug fix

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ async def slow_response(seconds: int = Query(10,  description="The number of sec
     start_time = time.time()
     await asyncio.sleep(seconds)
     end_time = time.time()
-    return {"message": f"slept {seconds} seconds, overhead {(int)(end_time - start_time * 1000) - seconds * 1000} ms"}
+    return {"message": f"slept {seconds} seconds, overhead {(int)(end_time - start_time) * 1000 - seconds * 1000} ms"}
